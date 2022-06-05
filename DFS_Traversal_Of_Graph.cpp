71b17e5bcc00ff4d770a25957e65e87c2c5a05ed
Anshika Maheshwari <83770244+anshi20@users.noreply.github.com>
2022-06-06 00:01:03 +0530

    Update and rename DFS_Traversal_Of_Graph.c++ to DFS_Traversal_Of_Graph.cpp

---
+++ DFS_Traversal_Of_Graph.cpp
@@ -0,0 +1,47 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define vi vector<int>
+const int N = 1e5 + 2;
+bool visited[N];
+vi adj[N];
+void dfs(int node)
+{
+    visited[node]=true;
+    cout<<node<<" ";
+    for(int x:adj[node])
+    {
+        if(visited[x]);
+        else
+        {
+            dfs(x);
+        }
+    }
+}
+int main()
+{
+    int n,m;
+    
+    
+    int i;
+    for(i=0;i<N;i++)
+    {
+        visited[i] = false;
+    }
+    cout<<"Enter no. of nodes : ";
+    cin>>n;
+    cout<<"Enter no. of edges : ";
+    cin>>m;
+    cout<<"Enter edges : \n";
+    for(i=0;i<m;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    dfs(1);
+    return 0;
+}
